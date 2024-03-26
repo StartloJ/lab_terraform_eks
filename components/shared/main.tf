e8c0860260edf79cbf8458405be119bc06f534a7
watcharin_start <watcharin.st11@gmail.com>
2024-03-26 14:06:48 +0700

    fixed lint

--- components/shared/main.tf
+++ components/shared/main.tf
@@ -3,7 +3,7 @@ locals {
   current_region = var.aws_region
 
   vpc_cidr       = var.vpc_cidr
-  num_of_subnets = min(length(data.aws_availability_zones.available.names), 3)
+  num_of_subnets = min(length(data.aws_availability_zones.available.names), 2)
   azs            = slice(data.aws_availability_zones.available.names, 0, local.num_of_subnets)
 
   argocd_secret_manager_name = var.argocd_secret_manager_name_suffix
